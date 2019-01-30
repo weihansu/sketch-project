2a2c678a3f8cbe5a80a32d9e61368587f9ceb5e6
weihansu <weihan.sw@gmail.com>
2019-01-30 04:00:43 +0000

    add comments to functions

--- chartProject.js
+++ chartProject.js
@@ -1,14 +1,24 @@
+/**
+  * @desc function to create DIVs and className
+  * @param string className
+  * @param string classApend
+  * @param number id
+  * @return DOM to create
+*/
 function createDiv(className, classApend, id) {
   var newDiv = document.createElement('div');
-  newDiv.className = className;
-  //newDiv.className = className + "_" + id;
+  newDiv.className = className + ' ' + className + '-' + id;
   classApend.appendChild(newDiv);
   return newDiv
 }
 
+/**
+  * @desc function to create a SPAN with value of data
+  * @param number value
+  * @param string classApend
+*/
 function createSpan(value, classApend) {
   var toSpan = document.createElement('SPAN');
-  //newDiv.className = className + "_" + id;
   var valuePlot = document.createTextNode(value);
   toSpan.appendChild(valuePlot);
   classApend.appendChild(toSpan);
